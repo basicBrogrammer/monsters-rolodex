d261a779ad92de2d3c05956a0b340100fff004b2
Jeremy Ward <jeremy.ward@mckesson.com>
2019-09-20 06:07:55 -0400

    Updates

---
+++ precache-manifest.702392016ea4f7bf5ad2766e7f966362.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20657a2d66e4e9b9b9944ce643a87d4f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1a8d21cddd35a7092ee6",
+    "url": "/monsters-rolodex/static/css/main.82371c4d.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "1a8d21cddd35a7092ee6",
+    "url": "/monsters-rolodex/static/js/main.ff96ea0b.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
